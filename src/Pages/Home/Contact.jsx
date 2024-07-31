179c7a251b73b7c7953783caf023cb7100a032b3
mahindipto <mahindipto18@gmail.com>
2024-08-01 03:03:55 +0600

    added email.js for contact

--- src/Pages/Home/Contact.jsx
+++ src/Pages/Home/Contact.jsx
@@ -1,5 +1,32 @@
+import emailjs from '@emailjs/browser';
+import { useRef } from 'react';
 
+
+// service_ur05fw6
 const Contact = () => {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(
+                'service_ur05fw6',
+                'template_g7xbg9l',
+                form.current, {
+                publicKey: 'dKzCB4t1t9ZgtE0LB',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+    };
+
     return (
         <div className="hero min-h-screen py-20">
             <div className="hero-content flex-col gap-7 lg:flex-row-reverse">
@@ -10,34 +37,34 @@ const Contact = () => {
                 </div>
                 <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100"
                 >
-                    <form className="card-body border-2 rounded-lg border-prime ">
+                    <form ref={form} onSubmit={sendEmail} className="card-body border-2 rounded-lg border-prime ">
                         <h1 className="text-3xl font-bold text-center">Get In Touch</h1>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Name</span>
                             </label>
-                            <input type="text" placeholder="Name" className="input input-bordered" required />
+                            <input type="text" name="from_name" placeholder="Name" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" placeholder="email" className="input input-bordered" required />
+                            <input type="email" name="from_email" placeholder="email" className="input input-bordered" required />
                         </div>
-                        <div className="form-control">
+                        {/* <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Subject</span>
                             </label>
                             <input type="subject" placeholder="subject" className="input input-bordered" required />
-                        </div>
+                        </div> */}
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Massage</span>
                             </label>
-                            <textarea placeholder="massage" className="input input-bordered h-32" required></textarea>
+                            <textarea name="message" placeholder="massage" className="input input-bordered h-32" required></textarea>
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn text-lg bg-prime text-white font-semibold hover:bg-white hover:text-prime">Submit</button>
+                            <input className="btn text-lg bg-prime text-white font-semibold hover:bg-white hover:text-prime" type="submit" value="Send" />
                         </div>
                     </form>
                 </div>
